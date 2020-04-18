a52f4adc4ef819d1d3f3f652ae5cc1fb03ea8c5c
Ramazan Alkan <business@ramazanalkan.com>
2020-04-18 20:32:21 +0300

    Update RConfig.php

--- RImageUpload/RConfig.php
+++ RImageUpload/RConfig.php
@@ -8,13 +8,11 @@
      * Author URI: https://ramazanalkan.com
      * Github URI: https://github.com/ramazan-alkan
      * Version: 1.0
-     * Documention URI: https//github.com/ramazan-alkan
-     * Licence: Open Source
+     * Documention URI: https//github.com/ramazan-alkan/php-compress-and-upload-image
     */
 
     $upload_folder = "../uploads"; // Image upload folder
     $max_file_size = 10; // Max file size (MB)
-    $file_types = array('jpg', 'png', 'JPG', 'PNG', 'jpeg', 'JPEG'); // Allowed image types
 
     # Made by ramazanalkan.com
 
